d6c8fa85d7ca3530a9d82a18e6b85f102876ac5f
amenesca <alanmenescal@hotmail.com>
2022-09-12 18:47:54 -0300

    tentar resolver o problema de recentralização

--- src/fractol_zoom.c
+++ src/fractol_zoom.c
@@ -14,6 +14,7 @@ void    fractol_zoom_in(t_fractol *fr)
 void    fractol_zoom_out(t_fractol *fr)
 {
     double zoom = 1.10;
+    
     fr->min_r = fr->min_r * zoom;
 	fr->max_r = fr->max_r * zoom;
 	fr->min_i = fr->min_i * zoom;
--- src/handle_keys.c
+++ src/handle_keys.c
@@ -10,10 +10,19 @@ int handle_keys(int button, t_fractol *fr)
 #include <stdio.h>
 int handle_mouse(int button, int x, int y, t_fractol *fr)
 {
-	printf("x:%d\ny:%d\nbutton:%d\n", x, y, button);
 	if (button == 1)
 	{
-		
+		double pointX = (((double)x * ((fr->max_r - fr->min_r) / WIN_X)));
+		double pointY = ((double)y *((fr->max_r - fr->min_r) / WIN_Y));
+		double centerX = fr->max_r - fr->min_r;
+		double centerY = fr->max_r - fr->min_r;
+		fr->min_r = pointX + centerX/2;
+		fr->max_r = pointX + centerX/2;
+		fr->min_i = pointY + centerY/2;
+		fr->max_i = fr->min_i + (fr->max_r - fr->min_r) * ((double)WIN_Y / (double)WIN_X); 
+    	fr->render = 0;
+		printf("testeX:%f\ntesteY:%f\n", pointX, pointY);
+		printf ("min_r:%f\nmax_r:%f\nmin_i:%f\nmax_i:%f\n", fr->min_r, fr->max_r, fr->min_i, fr->max_i);
 	}
 	if (button == 5)
 		fractol_zoom_in(fr);

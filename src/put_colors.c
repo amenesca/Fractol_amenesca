d7e2b983cd5fbeff6bb486702ef0e05e436f6007
amenesca <alanmenescal@hotmail.com>
2024-09-14 19:46:15 -0300

    src/put_colors.c optimization on hue calculus

--- src/put_colors.c
+++ src/put_colors.c
@@ -17,10 +17,9 @@ void	put_colors(t_fractol *fr)
 	t_color	colors;
 
 	colors.h = 2 * fr->iter;
-	colors.s = 0.5;
-	colors.v = 0.5;
-	while (colors.h > 360)
-		colors.h = colors.h - 360;
+	colors.s = 0.8;
+	colors.v = 0.6;
+	colors.h = fmod(colors.h, 360.0);
 	hsv_to_rgb(&colors);
 	ft_rgb(&colors);
 	my_mlx_pixel_put(fr, fr->x, fr->y, colors.rgb);
